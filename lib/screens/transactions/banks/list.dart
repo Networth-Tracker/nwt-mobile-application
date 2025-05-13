1650c87a4fff5beb1656cb604ac0c17a2d62a29b
NWT Dev <nwtdev@harshas-MacBook-Pro.local>
2025-05-13 18:02:51 +0530

    Added CICD Script

--- lib/screens/transactions/banks/list.dart
+++ lib/screens/transactions/banks/list.dart
@@ -1,15 +1,71 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nwt_app/constants/colors.dart';
+import 'package:nwt_app/constants/sizing.dart';
+import 'package:nwt_app/widgets/common/app_input_field.dart';
+import 'package:nwt_app/widgets/common/text_widget.dart';
 
 class BankTransactionListScreen extends StatefulWidget {
   const BankTransactionListScreen({super.key});
 
   @override
-  State<BankTransactionListScreen> createState() => _BankTransactionListScreenState();
+  State<BankTransactionListScreen> createState() =>
+      _BankTransactionListScreenState();
 }
 
 class _BankTransactionListScreenState extends State<BankTransactionListScreen> {
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(Icons.chevron_left),
+            ),
+            AppText(
+              "Transactions",
+              variant: AppTextVariant.headline6,
+              weight: AppTextWeight.semiBold,
+            ),
+            const Opacity(opacity: 0, child: Icon(Icons.chevron_left)),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppSizing.scaffoldHorizontalPadding,
+              ),
+              child: AppInputField(
+                controller: _searchController,
+                prefix: Icon(
+                  CupertinoIcons.search,
+                  color: AppColors.darkTextMuted,
+                ),
+                hintText: "Search...",
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
