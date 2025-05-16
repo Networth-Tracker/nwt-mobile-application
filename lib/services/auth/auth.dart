04f6ea0596396b8c098c7bc97f73e3e8e7231daa
NWT Dev <nwtdev@harshas-MacBook-Pro.local>
2025-05-16 12:50:18 +0530

    test-script

--- lib/services/auth/auth.dart
+++ lib/services/auth/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:nwt_app/constants/api.dart';
 import 'package:nwt_app/constants/storage_keys.dart';
@@ -22,7 +23,7 @@ class AuthService {
       final appSignature = await SmsAutoFill().getAppSignature;
       final response = await NetworkAPIHelper().post(ApiURLs.GENERATE_OTP, {
         "phonenumber": phoneNumber,
-        'apphash': appSignature,
+        'apphash': kIsWeb ? 'nwt-app' : appSignature,
       }); 
       if (response != null) {
         final responseData = jsonDecode(response.body);
